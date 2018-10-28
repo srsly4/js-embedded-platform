8b0b849df04625409132903b53eecf6c9e585a14
Szymon Piechaczek <piechaczek.szymon@gmail.com>
2018-10-28 15:39:22 +0100

    Separated CMake binary

--- .travis/platform-build.sh
+++ .travis/platform-build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-cmake . -DPLATFORM="$TARGET_PLATFORM"
+wget https://cmake.org/files/v3.12/cmake-3.12.3-Linux-x86_64.sh -O=cmake.sh
+chmod +x cmake.sh
+
+./cmake.sh . -DPLATFORM="$TARGET_PLATFORM"
 make
