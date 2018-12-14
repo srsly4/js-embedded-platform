87084c60e52ffce2968ad5741f907da972492ccc
Wiesław Stanek <wieslaw.stanek@spinney.io>
2018-12-14 23:11:01 +0100

    Update get-toolchain.sh

--- .travis/get-toolchain.sh
+++ .travis/get-toolchain.sh
@@ -6,7 +6,7 @@ cd RPi-tools
 git checkout 5caa7046982f0539cf5380f94da04b31129ed521
 
 RASPBERRY_SYSROOT=`pwd`/arm-bcm2708/arm-linux-gnueabihf/arm-linux-gnueabihf/sysroot
-RASPBERRY_COMPILERS=`pwd`/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin
+RASPBERRY_COMPILERS=`pwd`/arm-bcm2708/arm-linux-gnueabihf/bin
 
 echo "export RASPBERRY_SYSROOT=$RASPBERRY_SYSROOT" > ~/getenv.sh
 echo "export RASPBERRY_COMPILERS=$RASPBERRY_COMPILERS" >> ~/getenv.sh
